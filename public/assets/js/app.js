69b1e4a8410cb7f7a925787901e28ca747870fb6
Owen Roth <oroth8@comcast.net>
2020-10-25 12:51:18 -0500

    added full functionality from going over class notes. added file structure.

---
+++ public/assets/js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $.get("/api/notes",function(data){
+        // console.log(data);
+        for (let index = 0; index <data.length; index++) {
+           $(".container").append(`<div class="card">
+           <div class="card-body"> ${data[index].title} <span style="color: red" class="del" data-name=${data[index].title}>-</span></div>
+           </div>`);
+            
+        }
+    })
+
+    $(document).on("click",".del", function(){
+        // console.log(this);
+        var id = $(this).attr("data-name");
+        console.log(id);
+        $.ajax({
+            url:"/api/note/"+id,
+            method: "DELETE"
+        }).then(function(data){
+            console.log(data);
+        })
+    })
+    
+
+})
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const fs = require("fs");
+const { join } = require("path");
+const path = require("path");
+
+
+// const noteList = require("./db/noteList");
+// API route fill in
+// const apiRoutes = require(".routes/apiRoutes");
+// // HTML route fill in
+// const htmlRoutes = require(".routes/htmlRoutes");
+
+// Init app and port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// note taking object
+// testing data
+// const notes = [
+//     {
+//         title: "Server Notes",
+//         date: "10/25/2020",
+//         notes: "lorem stakmflksdjflk;ansdf;klasdnfm;las"
+//     }
+// ];
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// Allows use of static files: Important
+app.use(express.static("public"));
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
+//DISPLAY GETS 
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes",function(req,res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (req,res){
+    fs.readFile("./db/noteList.json", "utf8",function(err,data){
+        if(err) throw err;
+        console.log(data);
+        const newData = JSON.parse(data);
+        res.json(newData);
+    })
+    
+});
+// Taking user data POSTS
+app.post("/api/note", function (req,res){
+    fs.readFile("./db/noteList.json", "utf8",function(err,data){
+        if(err) throw err;
+        console.log(data);
+        const noteList = JSON.parse(data);
+        noteList.push(req.body);
+
+        fs.writeFile("./db/noteList.json",JSON.stringify(noteList),function(err){
+            if(err) throw err;
+            console.log("Added");
+            res.redirect("/notes");
+        });
+    });
+})
+
+app.delete("/api/note/:id", function(req,res){
+    res.json(req.params.title);
+})
+// Starts server on PORT
+app.listen(PORT, ()=> console.log(`Listening on PORT: ${PORT}`));
