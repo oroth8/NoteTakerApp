e757afc995a53589816f3c2316b9cb9c6f6114f9
Owen Roth <oroth8@comcast.net>
2020-10-25 18:11:36 -0500

    added footer and CSS elements

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
     $.get("/api/notes",function(data){
         // console.log(data);
         for (let index = 0; index <data.length; index++) {
-           $(".container").append(`<div class="card">
-           <div class="card-body"> ${data[index].title} <span style="color: red" class="del" data-name=${data[index].title}>-</span></div>
-           </div>`);
+            $(".container").append(`<div class="card" style="width: 70rem;">
+            <div class="card-body">
+              <h5 class="card-title">${data[index].title}<button type="button" class="btn btn-danger del" data-name=${data[index].title}>delete</button></h5>
+              <h6 class="card-subtitle mb-2 text-muted">${data[index].date}</h6>
+              <p class="card-text">${data[index].notes}</p>
+            </div>
+          </div>`)
+
+
+
+
+        //    $(".container").append(`<div class="card">
+        //    <div class="card-body"> ${data[index].title} <span style="color: red" class="del" data-name=${data[index].title}>-</span></div>
+        //    </div>`);
             
         }
     })
